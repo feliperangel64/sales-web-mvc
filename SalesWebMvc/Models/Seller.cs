9b2e8344b0369aac78595a7ac7111519af58c604
Felipe Rangel <felipe.rangel.64@gmail.com>
2020-10-17 21:23:11 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -12,6 +12,7 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecords> Sales { get; set; } = new List<SalesRecords>();
 
         public Seller()
